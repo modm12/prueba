70c2fc20f5c20ee890f6f14ff0cfceb3ef789888
est1522712 <est1522712@192.168.56.1>
2015-06-08 10:47:47 -0600

    cambio

--- src/github/Github.java
+++ src/github/Github.java
@@ -16,7 +16,7 @@ public class Github {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola Mundo");
+        System.out.println("hola Mundo loco");
     }
     
 }
